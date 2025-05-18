59b32b619a8db327cd025dab408009761e732fe6
Quds-Aljabri <qudsahmedaljabri@gmail.com>
2025-05-18 12:21:51 +0400

    multiply the second number withn the first

--- calcaulator.py
+++ calcaulator.py
@@ -18,7 +18,13 @@ elif  operator == "*":
     result = num1 * num2
 
 elif  operator == "**":
+    result = num2 * num1
+
+elif  operator == "**":
     result = num1 ** num2
+
+elif operator == "/":
+    result = num1 / num2
 
 elif  operator == "fac":
    answer = 1
