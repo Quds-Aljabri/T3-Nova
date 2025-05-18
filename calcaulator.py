ec177f693a1b64d5d460233338493322c22e6df6
Quds-Aljabri <qudsahmedaljabri@gmail.com>
2025-05-18 11:32:12 +0400

    Adding two numbers

---
+++ calcaulator.py
@@ -0,0 +1,14 @@
+num1 = 0
+num2 = 0
+operator = " "
+
+
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 1st number: "))
+
+operator = input("Enter the operator  : ")
+
+if  operator == "+":
+    result = num1 + num2
+
+print(result)
